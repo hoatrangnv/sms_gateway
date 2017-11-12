0a927763503f259c45383c4d28625617c217a466
namnv <namnv@hanelsoft.vn>
2017-11-12 21:44:50 +0700

    update

--- app/Library/AdminFunction/Define.php
+++ app/Library/AdminFunction/Define.php
@@ -72,7 +72,6 @@ class Define{
     const STATUS_SHOW = 1;
     const STATUS_HIDE = 0;
     const STATUS_BLOCK = -2;
-    const NAMTEST = -2;
 
     static $arrLanguage = array(Define::VIETNAM_LANGUAGE => 'vi',Define::ENGLISH_LANGUAGE => 'en');
 
