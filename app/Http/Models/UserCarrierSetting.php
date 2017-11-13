6e4e099ac089721f9320a6488c9a1269a1a8ebb1
quynhtm <manhquynh1984@gmail.com>
2017-11-13 22:32:27 +0700

    thêm các models

--- app/Http/Models/UserCarrierSetting.php
+++ app/Http/Models/UserCarrierSetting.php
@@ -8,21 +8,20 @@ use Illuminate\Support\Facades\DB;
 
 use App\library\AdminFunction\Define;
 
-class SystemSetting extends BaseModel
+class UserCarrierSetting extends BaseModel
 {
-    protected $table = Define::TABLE_SYSTEM_SETTING;
-    protected $primaryKey = 'system_setting_id';
+    protected $table = Define::TABLE_USER_CARRIER_SETTING;
+    protected $primaryKey = 'user_carrier_setting_id';
     public $timestamps = false;
 
-    protected $fillable = array('time_check_connect', 'concatenation_strings', 'concatenation_rule', 'api_manager', 'api_manager_en',
-        'api_customer', 'api_customer_en','system_content','system_content_en','created_date','updated_date');
+    protected $fillable = array('carrier_id', 'user_manager_id', 'user_customer_id', 'cost','created_date','updated_date');
 
     public static function createItem($data){
         try {
             DB::connection()->getPdo()->beginTransaction();
-            $checkData = new SystemSetting();
+            $checkData = new UserCarrierSetting();
             $fieldInput = $checkData->checkField($data);
-            $item = new SystemSetting();
+            $item = new UserCarrierSetting();
             if (is_array($fieldInput) && count($fieldInput) > 0) {
                 foreach ($fieldInput as $k => $v) {
                     $item->$k = $v;
@@ -31,8 +30,8 @@ class SystemSetting extends BaseModel
             $item->save();
 
             DB::connection()->getPdo()->commit();
-            self::removeCache($item->system_setting_id,$item);
-            return $item->system_setting_id;
+            self::removeCache($item->user_carrier_setting_id,$item);
+            return $item->user_carrier_setting_id;
         } catch (PDOException $e) {
             DB::connection()->getPdo()->rollBack();
             throw new PDOException();
@@ -42,15 +41,15 @@ class SystemSetting extends BaseModel
     public static function updateItem($id,$data){
         try {
             DB::connection()->getPdo()->beginTransaction();
-            $checkData = new SystemSetting();
+            $checkData = new UserCarrierSetting();
             $fieldInput = $checkData->checkField($data);
-            $item = SystemSetting::find($id);
+            $item = UserCarrierSetting::find($id);
             foreach ($fieldInput as $k => $v) {
                 $item->$k = $v;
             }
             $item->update();
             DB::connection()->getPdo()->commit();
-            self::removeCache($item->system_setting_id,$item);
+            self::removeCache($item->user_carrier_setting_id,$item);
             return true;
         } catch (PDOException $e) {
             //var_dump($e->getMessage());
@@ -76,12 +75,12 @@ class SystemSetting extends BaseModel
         if($id <= 0) return false;
         try {
             DB::connection()->getPdo()->beginTransaction();
-            $item = SystemSetting::find($id);
+            $item = UserCarrierSetting::find($id);
             if($item){
                 $item->delete();
             }
             DB::connection()->getPdo()->commit();
-            self::removeCache($item->system_setting_id,$item);
+            self::removeCache($item->user_carrier_setting_id,$item);
             return true;
         } catch (PDOException $e) {
             DB::connection()->getPdo()->rollBack();
@@ -93,13 +92,13 @@ class SystemSetting extends BaseModel
     public static function searchByCondition($dataSearch = array(), $limit =0, $offset=0, &$total){
 //        FunctionLib::debug($dataSearch);
         try{
-            $query = SystemSetting::where('system_setting_id','>',0);
+            $query = UserCarrierSetting::where('user_carrier_setting_id','>',0);
             if (isset($dataSearch['time_check_connect']) && $dataSearch['time_check_connect'] != '') {
                 $query->where('time_check_connect','LIKE', '%' . $dataSearch['time_check_connect'] . '%');
             }
 
             $total = $query->count();
-            $query->orderBy('system_setting_id', 'desc');
+            $query->orderBy('user_carrier_setting_id', 'desc');
 
             //get field can lay du lieu
             $fields = (isset($dataSearch['field_get']) && trim($dataSearch['field_get']) != '') ? explode(',',trim($dataSearch['field_get'])): array();
